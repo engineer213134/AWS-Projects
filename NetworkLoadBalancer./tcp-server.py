807427689937ddf3233b99f041b11b5a5df83e57
engineer213134 <66885586+engineer213134@users.noreply.github.com>
2022-12-26 11:41:34 -0800

    Update tcp-server.py

--- NetworkLoadBalancer./tcp-server.py
+++ NetworkLoadBalancer./tcp-server.py
@@ -7,10 +7,14 @@ TCP_IP = '0.0.0.0'
 TCP_PORT = 6381
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
+#Socket object see https://docs.python.org/3/library/socket.html , sock stream is a TCP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Binding ip and port to a socket instance
 s.bind((TCP_IP, TCP_PORT))
+#listen() method puts server in a listing mode
 s.listen(1)
 
+#https://stackoverflow.com/questions/65171871/what-is-conn-addr-s-accept-in-python-socket return variable conn/addr return pair value
 conn, addr = s.accept()
 print 'Connection address:', addr
 while 1:
